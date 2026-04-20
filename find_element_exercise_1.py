f5b49b649fcec9460453675f9ca62964bcb495e8
Diana Gabriel <gabrieldiana460@gmail.com>
2026-04-20 11:22:51 -0500

    Update find_element_exercise_1.py

--- find_element_exercise_1.py
+++ find_element_exercise_1.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
 
 # Buscar el título
-driver...
-
+title = driver.find_element(By.CSS_SELECTOR, ".auth-form__title")
+print(title.text)
 # Cerrar el navegador
-driver...
+driver.quit()
