bd82b930188bf0df6ca120e605f3cf4d77ba927f
Diana Gabriel <gabrieldiana460@gmail.com>
2026-04-20 11:45:27 -0500

    Update find_element_exercise_3.py

--- find_element_exercise_3.py
+++ find_element_exercise_3.py
@@ -6,12 +6,12 @@ driver = webdriver.Chrome()
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
 
 # Buscar elementos
-email = driver...
-password = driver...
+email = driver.find_element(By.ID, "email")
+password = driver.find_element(By.ID, "password")
 
 # Probar el atributo placeholder para cada elemento
-assert ...
-assert ...
+assert email.get_attribute("placeholder") == "Correo electrónico"
+assert password.get_attribute("placeholder") == "Contraseña"
 
 # Cerrar el navegador
-driver...
+driver.quit()
