ecb4f078921a062593377fce697b2b741c5f9728
Diana Gabriel <gabrieldiana460@gmail.com>
2026-04-20 11:33:19 -0500

    Update find_element_exercise_2.py

--- find_element_exercise_2.py
+++ find_element_exercise_2.py
@@ -6,10 +6,10 @@ driver = webdriver.Chrome()
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
 
 # Encontrar todos los elementos
-elements = driver...
+elements = driver.find_elements(By.XPATH, ".//img")
 
 # Comprobar que el número de elementos encontrados es mayor que 1 usando len()
-...
+assert len(images) > 1
 
 # Cerrar el navegador
-driver...
+driver.quit()
