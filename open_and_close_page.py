f6a0cb271196ff04625b730ea267dd981cd22f92
Diana Gabriel <gabrieldiana460@gmail.com>
2026-04-20 10:53:39 -0500

    Update Selenium script to open and verify URL

--- open_and_close_page.py
+++ open_and_close_page.py
@@ -4,10 +4,10 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 
 # Open the test stand page
-driver...
+driver.get('https://around-v1.nm.tripleten-services.com/signin?lng=es')
 
 # Check that /signin was added to the URL
-assert ...
+assert '/signin' in driver.current_url
 
 # Close the browser
-driver...
+driver.quit()
